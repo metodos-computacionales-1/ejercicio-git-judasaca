cb677f36aaf3e97e3d795997f2be1774d64cc76b
juan David <jd.salamancac@uniandes.edu.co>
2019-10-29 21:46:39 +0000

    punto 2

---
+++ 2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(void)
+{
+    float n1;
+    float n2;
+    float n3;
+    float pi=3.14159265359;
+    std::cout<<"el primer numero"<<std::endl;
+    std::cin>>n1;
+    std::cout<<"el segundo numero"<<std::endl;
+    std::cin>>n2;
+    std::cout<<"el tercer numero"<<std::endl;
+    std::cin>>n3;
+    
+    
+    return 0;
+}
