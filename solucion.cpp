0b7b00603cd926c648324513562d7aed23cee4d8
juan David <jd.salamancac@uniandes.edu.co>
2019-10-29 21:37:45 +0000

    perimetro y diamtro

--- solucion.cpp
+++ solucion.cpp
@@ -3,10 +3,11 @@
 int main(void)
 {
     float r;
+    float pi=3.14159265359;
     std::cout<<"ingrese el radio"<<std::endl;
     std::cin>>r;
-    std::cuot<<"el perimetro es "<<2*r<<std::endl
-    
+    std::cout<<"el perimetro es "<<2*r*pi<<std::endl;
+    std::cout<<"el diametro es "<<2*r<<std::endl;
     
     
     
