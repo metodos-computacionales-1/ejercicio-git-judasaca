0005e44a9a71db642a8bd8d477651557f9b0e867
juan David <jd.salamancac@uniandes.edu.co>
2019-10-30 22:43:55 +0000

    Solucion final

--- 2.cpp
+++ 2.cpp
@@ -2,9 +2,9 @@
 
 int main(void)
 {
-    float n1;
-    float n2;
-    float n3;
+    int n1;
+    int n2;
+    int n3;
     float pi=3.14159265359;
     std::cout<<"el primer numero"<<std::endl;
     std::cin>>n1;
@@ -13,6 +13,40 @@ int main(void)
     std::cout<<"el tercer numero"<<std::endl;
     std::cin>>n3;
     
+    if(n1>n2 && n1>n3){
+        if(n1*n1==((n2*n2)+(n3*n3))){
+            std::cout<<"Los lados corresponden a un triangulo rectangulo"<<std::endl;
+                      
+        }
+        else{
+            std::cout<<"Los lados NO corresponden a un triangulo rectangulo"<<std::endl; 
+        }
+        
+            
+        }
+        
     
+    if(n2>n1 && n2>n3){
+        if(n2*n2==((n3*n3)+(n1*n1))){
+            std::cout<<"Los lados corresponden a un triangulo rectangulo"<<std::endl;
+                      
+        }
+        else{
+            std::cout<<"Los lados NO corresponden a un triangulo rectangulo"<<std::endl; 
+        }
+        
+    }
+    else{
+        if(n3*n3==((n2*n2)+(n1*n1))){
+            std::cout<<"Los lados corresponden a un triangulo rectangulo"<<std::endl;
+                      
+        }
+        else{
+            std::cout<<"Los lados NO corresponden a un triangulo rectangulo"<<std::endl; 
+        }
+        
+        
+        
+    }
     return 0;
 }
